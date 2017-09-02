4e0f13e3da5f1757328e9121f307eb693568b96e
Josh Loftis <jshlfts32@gmail.com>
2017-09-01 23:54:53 -0400

    Pushing latest star wars and reverting hangman from accidentally overwriting it

--- Star-Wars-Game/assets/javascript/game.js
+++ Star-Wars-Game/assets/javascript/game.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
+// Global Variables
+//-----------------------------------------------------------------------------
   //object to hold characters, attack power, hp, and counter attack power
   var charAttr = {
     image: ['src="assets/images/Luke_Skywalker.png"', 'src="assets/images/Darthvader3.png"', 'src="assets/images/rey.png"', 'src="assets/images/kyloren.png"' ],
-    name: ["Luke", "Vader", "Rey", "Ren"],
+    name: ["Luke Skywalker", "Darth Vader", "Rey Skywalker", "Kylo Ren"],
     attack: [8, 10, 5, 4],
     health: [120, 100, 150, 180],
     counter: [10, 15, 8, 22]
@@ -10,22 +11,44 @@ $(document).ready(function() {
   var playerChosen = false;
   var enemyChosen = false;
   var gameOver = false;
-  var chosenChar;
+  var chosenPlayer;
   var chosenEnemy;
 
-  //iterates through each .charDiv and applies the corresponding charAttr key and value and then appends the characters name, image and health
+
+
+
+// Functions
+//-----------------------------------------------------------------------------
+
   $('.charDiv').each(function(i) {
-    $(this).attr({"name" : charAttr.name[i], "attack" : charAttr.attack[i], "health" : charAttr.health[i], "counter" : charAttr.counter[i]})
+    $(this).data({"name" : charAttr.name[i], "attack" : charAttr.attack[i], "health" : charAttr.health[i], "counter" : charAttr.counter[i]})
     .append('<div class="name">' + charAttr.name[i] + "</div>")
-    .append('<img class="img-responsive center-block" ' + charAttr.image[i] + '>')
+    .append('<img class="img-responsive center-block charImg" ' + charAttr.image[i] + '>')
     .append('<div class="health">' + charAttr.health[i] + "</div>");
   });
 
-  console.log($('#char1').attr("name"));
+
+  
+
+// Game Logic
+//-----------------------------------------------------------------------------
+$(document).ready(function() {
 
   $('.charDiv').on('click', function () {
-    console.log($(this).attr("name"));
-
-    playerChosen = true;
+    if (playerChosen == false) {
+      chosenPlayer = $(this);
+      $('#playerArea').append(this);
+      $(this).unbind('click');
+      $('.charDiv').not(this).appendTo('#holdingBin');
+      playerChosen = true;
+    } else if (enemyChosen == true){
+    } else {
+      $('#defenderArea').html(this);
+      $(this).unbind('click');
+      enemyChosen = true;
+      chosenEnemy = $(this);
+    }
   });
+
+
 });
